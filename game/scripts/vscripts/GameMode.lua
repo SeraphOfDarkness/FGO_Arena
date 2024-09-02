96dc9c85c447724feed1d5348dd21cc5b4875115
SeraphOfDarkness <121141380+SeraphOfDarkness@users.noreply.github.com>
2024-09-02 20:06:12 +0900

    team assignment

--- game/scripts/vscripts/GameMode.lua
+++ game/scripts/vscripts/GameMode.lua
@@ -6,6 +6,71 @@ ____exports.AddonGameMode = __TS__Class()
 local AddonGameMode = ____exports.AddonGameMode
 AddonGameMode.name = "AddonGameMode"
 function AddonGameMode.prototype.____constructor(self)
+    self.PlayersPerTeam = 0
+    self.GameMode = GameRules:GetGameModeEntity()
+    self:RegisterEventListeners()
+    self:DefaultConfiguration()
+end
+function AddonGameMode.prototype.DefaultConfiguration(self)
+    GameRules:SetCustomGameTeamMaxPlayers(DOTA_TEAM_GOODGUYS, self.PlayersPerTeam)
+    GameRules:SetCustomGameTeamMaxPlayers(DOTA_TEAM_BADGUYS, self.PlayersPerTeam)
+end
+function AddonGameMode.prototype.Setup3v3v3v3(self)
+    self.PlayersPerTeam = 3
+    do
+        local i = DOTA_TEAM_CUSTOM_1
+        while i <= DOTA_TEAM_CUSTOM_4 do
+            GameRules:SetCustomGameTeamMaxPlayers(i, self.PlayersPerTeam)
+            i = i + 1
+        end
+    end
+end
+function AddonGameMode.prototype.Setup4v4v4(self)
+    self.PlayersPerTeam = 4
+    do
+        local i = DOTA_TEAM_CUSTOM_1
+        while i <= DOTA_TEAM_CUSTOM_3 do
+            GameRules:SetCustomGameTeamMaxPlayers(i, self.PlayersPerTeam)
+            i = i + 1
+        end
+    end
+end
+function AddonGameMode.prototype.OnGameStateChange(self)
+    local CurrentState = GameRules:State_Get()
+    repeat
+        local ____switch9 = CurrentState
+        local ____cond9 = ____switch9 == DOTA_GAMERULES_STATE_CUSTOM_GAME_SETUP
+        if ____cond9 then
+            do
+                if IsInToolsMode() then
+                    self:DebugConfiguration()
+                end
+                break
+            end
+        end
+        ____cond9 = ____cond9 or ____switch9 == DOTA_GAMERULES_STATE_WAIT_FOR_MAP_TO_LOAD
+        if ____cond9 then
+            do
+                break
+            end
+        end
+        do
+            do
+                break
+            end
+        end
+    until true
+end
+function AddonGameMode.prototype.DebugConfiguration(self)
+    self:Setup3v3v3v3()
+    PlayerResource:SetCustomTeamAssignment(0, DOTA_TEAM_CUSTOM_1)
+end
+function AddonGameMode.prototype.RegisterEventListeners(self)
+    ListenToGameEvent(
+        "game_rules_state_change",
+        function() return self:OnGameStateChange() end,
+        nil
+    )
 end
 function AddonGameMode.Precache(context)
 end
--- src/vscripts/GameMode.ts
+++ src/vscripts/GameMode.ts
@@ -1,3 +1,5 @@
+//import * as Utils from "./utils/UtilList"
+
 declare global 
 {
     interface CDOTAGameRules 
@@ -8,6 +10,76 @@ declare global
 
 export class AddonGameMode 
 {
+    PlayersPerTeam = 0;
+    GameMode: CDOTABaseGameMode;
+
+    DefaultConfiguration(): void
+    {
+        GameRules.SetCustomGameTeamMaxPlayers(DotaTeam.GOODGUYS, this.PlayersPerTeam);
+        GameRules.SetCustomGameTeamMaxPlayers(DotaTeam.BADGUYS, this.PlayersPerTeam);
+    }
+
+    Setup3v3v3v3(): void
+    {
+        this.PlayersPerTeam = 3;
+        for (let i = DotaTeam.CUSTOM_1; i <= DotaTeam.CUSTOM_4; i++)
+        {
+            GameRules.SetCustomGameTeamMaxPlayers(i, this.PlayersPerTeam);
+        }
+    }
+
+    Setup4v4v4(): void
+    {
+        this.PlayersPerTeam = 4;
+        for (let i = DotaTeam.CUSTOM_1; i <= DotaTeam.CUSTOM_3; i++)
+        {
+            GameRules.SetCustomGameTeamMaxPlayers(i, this.PlayersPerTeam);
+        }
+    }
+
+    OnGameStateChange(): void
+    {
+        const CurrentState = GameRules.State_Get();
+
+        switch (CurrentState)
+        {
+            case GameState.CUSTOM_GAME_SETUP:
+            {
+                if (IsInToolsMode())
+                {
+                    this.DebugConfiguration();
+                }
+                break;
+            }
+            case GameState.WAIT_FOR_MAP_TO_LOAD:
+            {
+                break;
+            }
+            default: 
+            {
+                break;
+            }
+        }
+    }
+
+    DebugConfiguration(): void
+    {
+        this.Setup3v3v3v3();
+        PlayerResource.SetCustomTeamAssignment(0, DotaTeam.CUSTOM_1);
+    }
+
+    RegisterEventListeners(): void
+    {
+        ListenToGameEvent("game_rules_state_change", () => this.OnGameStateChange(), undefined);
+    }
+
+    constructor()
+    {
+        this.GameMode = GameRules.GetGameModeEntity();
+        this.RegisterEventListeners();
+        this.DefaultConfiguration();
+    }
+
     public static Precache(this: void, context: CScriptPrecacheContext) 
     {
     }
